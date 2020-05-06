f1880c6cb54b1facca10ca059d894436affd3d08
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-05-05 22:06:39 -0600

    use ?char syntax to complete rna transaction kata

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -7,7 +7,16 @@ defmodule RnaTranscription do
   iex> RnaTranscription.to_rna('ACTG')
   'UGAC'
   """
+  def convert_rna_char(char) do
+    case char do
+     ?G -> 'C'
+     ?C -> 'G'
+     ?T -> 'A'
+     ?A -> 'U'
+    end
+  end
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    List.to_charlist(Enum.map(dna, fn each -> convert_rna_char(each) end))
   end
 end
