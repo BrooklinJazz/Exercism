40b63362138189a575c98808d713bb19a8a8dc55
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-05-15 17:02:15 -0600

    Word Count Filter Punct

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,5 +6,21 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    sentence
+    |> filter_punctuation
+    |> split_sentence
+    |> count_words
+  end
+
+  def filter_punctuation(sentence) do
+    Regex.replace( ~r/^[[:punct:]]+$/, sentence, " ")
+  end
+
+  def split_sentence(sentence) do
+    String.split(sentence)
+  end
+
+  def count_words(word_list) do
+    Enum.reduce(word_list, %{}, fn each, total -> Map.update(total, each, 1, &(&1 + 1)) end)
   end
 end
